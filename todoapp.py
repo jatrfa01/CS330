a3d023d23bde193daeecd159b8ae6c0fb68224be
eshete <tarees01@luther.edu>
2016-05-16 12:29:04 -0500

    rgregergr

--- todoapp.py
+++ todoapp.py
@@ -8,8 +8,8 @@ app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
 
 from views import *
 
-from models import db
-db.create_all()
+#from models import db
+#db.create_all()
 
 
 if __name__ == '__main__':
@@ -18,11 +18,7 @@ if __name__ == '__main__':
 	port = int(os.environ.get("PORT", 5000))
 	app.run(host='0.0.0.0', port=port, debug=True)
 
-	#http.listen(process.env.PORT || 3000, function(){
-		#console.log('listening on', http.address().port);
-		#});
-
-
+	
 
 
 	#postgres://paqumbaesgrokz:cpMJAC5HNnXjX28mQHwSh2LnGN@ec2-54-225-91-215.compute-1.amazonaws.com:5432/dcn82n11q37968
