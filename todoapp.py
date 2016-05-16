b7986f3b7e0a2f7021ef1b255d6e43174087a52b
eshete <tarees01@luther.edu>
2016-05-16 11:11:18 -0500

    sdkgfhsdkjghdfkjs

--- todoapp.py
+++ todoapp.py
@@ -1,7 +1,8 @@
 from flask import Flask
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://patrycja:mypassword@localhost/todoapp'
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://patrycja:mypassword@localhost/todoapp'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://paqumbaesgrokz:cpMJAC5HNnXjX28mQHwSh2LnGN@ec2-54-225-91-215.compute-1.amazonaws.com:5432/dcn82n11q37968'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = 'False'
 app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
 
@@ -16,6 +17,11 @@ if __name__ == '__main__':
 
 	port = int(os.environ.get("PORT", 5000))
 	app.run(host='0.0.0.0', port=port, debug=True)
+
+	#http.listen(process.env.PORT || 3000, function(){
+		#console.log('listening on', http.address().port);
+		#});
+
 
 
 
