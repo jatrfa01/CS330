1d8098f8b5760906e0f0363a977b5839c51589ca
eshete <tarees01@luther.edu>
2016-05-16 12:38:49 -0500

    change the sql

--- todoapp.py
+++ todoapp.py
@@ -1,7 +1,7 @@
 from flask import Flask
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://patrycja:mypassword@localhost/todoapp'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://patrycja:mypassword@localhost/todoapp'
 #app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://paqumbaesgrokz:cpMJAC5HNnXjX28mQHwSh2LnGN@ec2-54-225-91-215.compute-1.amazonaws.com:5432/dcn82n11q37968'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = 'False'
 app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
